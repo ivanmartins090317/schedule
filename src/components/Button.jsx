b7f4fb2c4f4bf48c33d4026b6817062c2b5f2fcf
Ivan Martins <91843801+ivanmartins090317@users.noreply.github.com>
2022-03-04 16:23:57 -0300

    Add files via upload

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './button.css'
+
+const Button = ({children, onClick}) => {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  )
+}
+
+export default Button
